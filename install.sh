c12ff726787c5d7c01301e0315a87712001e0c9d
Sebastien Dumetz <s.dumetz@holusion.com>
2015-07-29 15:40:28 +0200

    initial base setup

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt-get -y install git \
+	build-essential \
+	curl \
+	apt-transport-https \
+	lsb-release
+
---
+++ node.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash -
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+sudo echo 'deb https://deb.nodesource.com/node_0.12 jessie main' > /etc/apt/sources.list.d/nodesource.list
+sudo apt-get install --yes nodejs
