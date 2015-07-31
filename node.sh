07dcdaedd5ceaa817b5b30958916b3d235a2e05c
Sebastien Dumetz <s.dumetz@holusion.com>
2015-07-31 09:49:04 +0200

    globally installed node modules

--- node.sh
+++ node.sh
@@ -3,3 +3,5 @@
 curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
 sudo echo 'deb https://deb.nodesource.com/node_0.12 jessie main' > /etc/apt/sources.list.d/nodesource.list
 sudo apt-get install --yes nodejs
+
+sudo npm install -g node-gyp grunt-cli
